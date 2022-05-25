955d35392991a086b846b25521f1592a74d21353
jmp-home <contact@jmpalazzolo.io>
2022-05-25 16:23:54 +0200

    LatinoNet Clean Architecture Project

---
+++ LatinoNetCleanArch/LatinoNet.RepositoryEFCore/DataContext/LatinoNetContext.cs
@@ -0,0 +1,15 @@
+﻿using LatinoNet.Entities.POCOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace LatinoNet.RepositoryEFCore.DataContext
+{
+    public  class LatinoNetContext : DbContext
+    { 
+        public LatinoNetContext(DbContextOptions<LatinoNetContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ LatinoNetCleanArch/LatinoNet.RepositoryEFCore/DataContext/LatinoNetContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatinoNet.RepositoryEFCore.DataContext
+{
+    internal class LatinoNetContextFactory : IDesignTimeDbContextFactory<LatinoNetContext>
+    {
+        public LatinoNetContext CreateDbContext(string[] args)
+        {
+            var OptionBuilder = new DbContextOptionsBuilder<LatinoNetContext>();
+            OptionBuilder.UseSqlServer("server=(localdb)\\mssqllocaldb;database=LatinoNet");
+            return new LatinoNetContext(OptionBuilder.Options);
+        }
+    }
+}
