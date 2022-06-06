d4f459895dc698a16685d27832df6cbf2425de81
Juan M Palazzolo <juanmartinpalazzolo@icloud.com>
2022-06-07 01:56:03 +0200

    Higher Order Functions

---
+++ CSharpRainerStropek/HigherOrderFunctions/HigherOrderFunctions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+Func<int, int> calculator = CreateCalculator();
+Console.WriteLine(calculator(1)); // Call happens outside scope of CreateCalculator()
+
+// Factory Pattern
+// This function returns a delegate
+Func<int, int> CreateCalculator()
+{
+    var factor = 42; // The factor gets promoted to the heap (closure)
+    return n => n * factor; // The lambda method captures factor
+}
+
+// Behind the scenes C# makes the following
+BestFriends CreateCalculatorInternal()
+{
+    return new BestFriends
+    {
+        factor = 42
+    };
+}
+
+class BestFriends
+{
+    public int factor;
+    public int Calculator(int n) => n * factor;
+}
